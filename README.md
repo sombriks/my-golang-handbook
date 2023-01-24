00879825b2a9311911d7cc93d50e8efec7172987
sombriks <sombriks@gmail.com>
2023-01-23 22:17:35 -0300

    check triangles/ read file

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # My Golang Handbook
 
-Personal golang exercises from [freecodecamp](https://www.freecodecamp.org/news/go-beginners-handbook/)
-and various sources
+Personal golang exercises from various sources
 
 ## How to run each sample code
 
@@ -15,7 +14,11 @@ I am looking at general operations that can be sampled on small snippets.
 I am not structuring big brains project configs, there will be other projects
 just to do that.
 
-## Additional study sources
+## Study sources
 
+- <https://www.freecodecamp.org/news/go-beginners-handbook/>
 - <https://gobyexample.com/command-line-arguments>
 - <https://pkg.go.dev/fmt>
+- <https://golangdocs.com/>
+- <https://www.golangprograms.com/>
+- <https://zetcode.com/all/#go>
--- exercises/0004-check-triangles/check-triangles.go
+++ exercises/0004-check-triangles/check-triangles.go
@@ -1,6 +1,42 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 // Reads a file called triangles.txt and tells which lines represents a triangle
 func main() {
 
+	textFile, err := os.Open("triangles.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	textFileScanner := bufio.NewScanner(textFile)
+	textFileScanner.Split(bufio.ScanLines)
+
+	for textFileScanner.Scan() {
+		var line = textFileScanner.Text()
+		// https://www.geeksforgeeks.org/how-to-split-a-string-in-golang/
+		var strNumbers = strings.Split(line, " ")
+		if len(strNumbers) != 3 {
+			fmt.Printf("line [%s] isn't valid and will be ignored\n", line)
+		} else {
+			a, _ := strconv.Atoi(strNumbers[0])
+			b, _ := strconv.Atoi(strNumbers[1])
+			c, _ := strconv.Atoi(strNumbers[2])
+			if a+b > c && a+c > b && b+c > a {
+				fmt.Printf("%s can form a triangle\n", strNumbers)
+			} else {
+				fmt.Printf("%s can't form a triangle\n", strNumbers)
+			}
+		}
+	}
+
+	_ = textFile.Close()
 }
